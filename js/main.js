053dc6397c317b5c77e8d0a508b699ada34b7481
Senad Uka <senad.uka@gmail.com>
2016-05-14 07:50:15 +0200

    slowed down ratio of listening to the event

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 var count = 0;
+var pressing = false;
 
-$(document).ready(function () {
+$(document).ready(function() {
   $("#tekst").bigText({
     horizontalAlign: 'left'
   });
 
+  $('#tekst').html(count);
+
   $("html").keyup(function(event) {
-    count += 10;
-    if (count > 100) count = 0;
-    $('#tekst').html(count);
+    pressing = true;
+    setTimeout(function() {
+      if (pressing) {
+        count += 10;
+        if (count > 100) count = 0;
+        $('#tekst').html(count);
+        pressing = false;
+      }
+    }, 500);
   })
 });
